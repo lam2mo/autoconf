c551ac5167f9440c07e4b97262a72e7cc243b469
Mike Lam <lam2mo@jmu.edu>
2018-02-28 17:46:21 -0500

    Clean script, merge source file, and add minimal autoconf sequence

--- run.sh
+++ run.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-# make src folder
+# clean and recreate src folder
 rm -rf src
 mkdir -p src
 
-# copy source file
-cp test.c src/test.c
-
-# set up Makefile.am
-echo "bin_PROGRAMS=test"    >src/Makefile.am
-echo "test_SOURCES=test.c"  >>src/Makefile.am
+# set up source file
+cat << EOF > src/test.c
+#include <stdio.h>
+int main()
+{
+    printf("hi!\n");
+    return 0;
+}
+EOF
 
 # set up configure.ac
-echo "AC_INIT([test], [1.0], [lam2mo@jmu.edu])"     > src/configure.ac
+echo "AC_INIT([test], [1.0], [lam2mo@jmu.edu])"     >  src/configure.ac
 echo "AM_INIT_AUTOMAKE([-Wall])"                    >> src/configure.ac
 echo "AC_PROG_CC"                                   >> src/configure.ac
 echo "AC_CONFIG_FILES([Makefile])"                  >> src/configure.ac
 echo "AC_OUTPUT"                                    >> src/configure.ac
 
-# files that are apparently necessary
+# set up Makefile.am
+echo "bin_PROGRAMS=test"    >  src/Makefile.am
+echo "test_SOURCES=test.c"  >> src/Makefile.am
+
+# files that are apparently necessary for automake
 touch src/NEWS
 touch src/README
 touch src/AUTHORS
-touch src/INSTALL
-touch src/COPYING
 touch src/ChangeLog
 
-# run autoconf
-(cd src && autoreconf --install)
+# run minimal autoconf sequence or swap w/ commented-out line to use autoreconf
+(cd src && aclocal && autoconf && automake --add-missing)
+#(cd src && autoreconf --install)
 
